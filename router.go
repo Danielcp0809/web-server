cbe2231b58d623025a21e55bffbd7109258ccb9d
Daniel0809 <jdany0809@hotmail.com>
2021-06-13 15:11:04 -0500

    Implementacion de rutas HTTP mediante el archivo handlers.go

--- router.go
+++ router.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -16,5 +15,15 @@ func NewRouter() *Router {
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(w, "Hello World, All it's ok")
+	handler, exist := r.FindHandler(request.URL.Path)
+	if !exist {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	handler(w, request)
+}
+
+func (r *Router) FindHandler(path string) (http.HandlerFunc, bool) {
+	handler, exist := r.rules[path]
+	return handler, exist
 }
