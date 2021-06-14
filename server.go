5c3cff85881de96f207411987ef7f33a280f5ee5
Daniel0809 <jdany0809@hotmail.com>
2021-06-14 18:22:08 -0500

    Implementacion de lectura y escritura de Json en GO con ayuda del handler userPostRequest

--- server.go
+++ server.go
@@ -30,6 +30,10 @@ func (s *Server) AddMiddleware(f http.HandlerFunc, middlewares ...Middleware) ht
 	return f
 }
 
-func (s *Server) Handle(path string, handler http.HandlerFunc) {
-	s.router.rules[path] = handler
+func (s *Server) Handle(method string, path string, handler http.HandlerFunc) {
+	_, exist := s.router.rules[path]
+	if !exist {
+		s.router.rules[path] = make(map[string]http.HandlerFunc)
+	}
+	s.router.rules[path][method] = handler
 }
